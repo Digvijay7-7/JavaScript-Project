c47ef5b94ccb19c0b90bb692d3c62f817ed51260
Digvijay Patil <digwijaypatil6521@gmail.com>
2023-10-07 18:34:06 +0530

    changes

--- internetStatusChecker/ats.js
+++ internetStatusChecker/ats.js
@@ -1,1 +1,20 @@
-window.addEventListener("load",checkInter)
+window.addEventListener("load", checkInternetStatus);
+
+function checkInternetStatus(){
+    const statusText = document.getElementById('statusText')
+    const ipAddressText = document.getElementById('ipAddressText')
+    const networkStrengthText = document.getElementById('networkStrengthText')
+
+    statusText.textContent = 'Checking...'
+
+    if(navigator.onLine){
+        
+
+    }
+    else{
+        statusText.textContent = 'Disconnected'
+        ipAddressText.textContent = '-'
+        networkStrengthText.textContent = '-'
+
+    }
+}
