1c15f6017ded2e61a14cf387abf2351fa1d24e2f
Digvijay Patil <digwijaypatil6521@gmail.com>
2023-10-30 23:32:54 +0530

    commit

---
+++ RGBColorGenerator/script.js
@@ -0,0 +1,12 @@
+const redSlider = document.getElementById("redSlider");
+const greenSlider = document.getElementById("greenSlider");
+const blueSlider = document.getElementById("blueSlider");
+
+
+const redValueSpan = document.getElementById("redvalue");
+const greenValueSpan = document.getElementById("greenvalue");
+const blueValueSpan = document.getElementById("bluevalue");
+
+const colorBox = document.getElementById("color-box");
+const copyButton = document.getElementById("copyButton");
+const inputTypeRGBValue = document.getElementById("input-type-value");
