acce48c706a6574060ce457fc05c9b02f9d1d4d0
Digvijay Patil <digwijaypatil6521@gmail.com>
2023-10-31 13:18:07 +0530

    commit

--- RGBColorGenerator/script.js
+++ RGBColorGenerator/script.js
@@ -9,4 +9,28 @@ const blueValueSpan = document.getElementById("bluevalue");
 
 const colorBox = document.getElementById("color-box");
 const copyButton = document.getElementById("copyButton");
-const inputTypeRGBValue = document.getElementById("input-type-value");
+const inputTypeRGBValue = document.getElementById("inputType");
+
+redSlider.addEventListener('input', updateColor);
+greenSlider.addEventListener('input', updateColor);
+blueSlider.addEventListener('input', updateColor);
+
+function updateColor(){
+    const redvalue = redSlider.value;
+    const greenvalue = greenSlider.value;
+    const bluevalue = blueSlider.value;
+    // console.log(redvalue, greenvalue, bluevalue);
+
+    const rgbColor = `rgb(${redvalue}, ${greenvalue}, ${bluevalue})`;
+    console.log(rgbColor);
+
+    colorBox.style.backgroundColor = rgbColor
+    redValueSpan.textContent = redvalue
+    greenValueSpan.textContent = greenvalue
+    blueValueSpan.textContent = bluevalue
+
+    inputTypeRGBValue.textContent = rgbColor
+
+}
+
+updateColor();
